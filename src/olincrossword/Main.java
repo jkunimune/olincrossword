7b12ef3188206f0baaa7e5f1579ed60c76753d1b
Justin Kunimune <justinkunimune@gmail.com>
2019-02-18 12:17:07 -0500

    The worst crossword ever.
    Now it fills the crossword in a reasonable order. It also has no idea what a word is, so it just fills it with meaningless numbers.

--- src/olincrossword/Main.java
+++ src/olincrossword/Main.java
@@ -26,6 +26,10 @@ package olincrossword;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * The main thing; make and print out a crossword puzzle!
@@ -36,18 +40,100 @@ public class Main {
 	
 	private static final int SIZE = 21;
 	
+	
 	public static void main(String[] args) throws IOException {
 		final char[][] grid = loadInitialState();
+		final Map<Integer, Integer> hist = new LinkedHashMap<Integer, Integer>();
 		
 		System.out.println(toString(grid));
+		
+		while (true) {
+			int maxL = 0, maxN = 0; // properties of best slot
+			int bestI = 0, bestJ = 0; // location of best slot
+			int bestAcr = 0; // orientation of best slot
+			for (int i = 1; i < grid.length-1; i ++) { // look at all the slots
+				for (int j = 1; j < grid[i].length-1; j ++) {
+					if (grid[i][j] != '#' && grid[i-1][j] == '#' && grid[i+1][j] != '#') { // across?
+						if (!hist.containsKey(0x10000|i<<8|j)) { // make sure we haven't filled this one
+							int l = 0, n = 0;
+							while (grid[i+l][j] != '#') {
+								if (grid[i+l][j] != ' ')
+									n ++; // count the intersections
+								l ++; // and the length
+							}
+							if (n > maxN || (n == maxN && l > maxL)) { // if it has more intersections
+								bestI = i; // save it
+								bestJ = j;
+								bestAcr = 1;
+								maxL = l;
+								maxN = n;
+							}
+						}
+					}
+					
+					if (grid[i][j] != '#' && grid[i][j-1] == '#' && grid[i][j+1] != '#') { // down?
+						if (!hist.containsKey(0x00000|i<<8|j)) { // make sure we haven't filled this one
+							int l = 0, n = 0;
+							while (grid[i][j+l] != '#') {
+								if (grid[i][j+l] != ' ')
+									n ++; // count the intersections
+								l ++; // and the length
+							}
+							if (n > maxN || (n == maxN && l > maxL)) { // if it has more intersections
+								bestI = i; // save it
+								bestJ = j;
+								bestAcr = 0;
+								maxL = l;
+								maxN = n;
+							}
+						}
+					}
+				}
+			}
+			
+			if (maxL == 0) // if we didn't find a single slot to fill
+				break;
+			
+			String word = chooseWord(grid, maxL, bestI, bestJ, bestAcr);
+			for (int k = 0; k < maxL; k ++)
+				if (bestAcr>0)
+					grid[bestI+k][bestJ] = word.charAt(k);
+				else
+					grid[bestI][bestJ+k] = word.charAt(k);
+			hist.put(bestAcr<<16|bestI<<8|bestJ, 0);
+			
+			System.out.println(toString(grid));
+		}
+		
+		System.out.print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 	}
 	
+	
+	private static String
+			chooseWord(char[][] grid, int len, int i, int j, int across) {
+		String x = "";
+		for (int k = 0; k < len; k ++)
+			x += k;
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO: Handle this
+			e.printStackTrace();
+		}
+		return x;
+	}
+	
+	
 	private static char[][] loadInitialState() throws IOException {
-		char[][] out = new char[SIZE][SIZE];
+		char[][] out = new char[SIZE+2][SIZE+2];
+		for (int i = 0; i < SIZE+2; i ++)
+			for (int j = 0; j < SIZE+2; j ++)
+				out[i][j] = '#';
+		
 		BufferedReader in = new BufferedReader(new FileReader("res/init.txt"));
 		try {
-			for (int i = 0; i < SIZE; i ++) {
-				for (int j = 0; j < SIZE; j ++) {
+			for (int i = 1; i < SIZE+1; i ++) {
+				for (int j = 1; j < SIZE+1; j ++) {
 					out[i][j] = (char)in.read();
 				}
 				in.read();
@@ -58,24 +144,16 @@ public class Main {
 		return out;
 	}
 	
+	
 	private static String toString(char[][] arr) {
 		String out = "";
-		out += "+";
-		for (int i = 0; i < arr.length; i ++)
-			out += "--";
-		out += "+\n";
 		for (char[] row: arr) {
-			out += "|";
 			for (char c: row) {
 				out += c;
 				out += " ";
 			}
-			out += "|\n";
+			out += "\n";
 		}
-		out += "+";
-		for (int i = 0; i < arr.length; i ++)
-			out += "--";
-		out += "+\n";
 		return out;
 	}
 }
